8045a0cbd3334c6d75688b7eadf0bd7fc370c37d
Ryan Arana <ryan@ryanarana.com>
2014-09-21 13:26:59 -0700

    refactor ship into its own class. and then make two of 'em!

--- main.lua
+++ main.lua
@@ -4,16 +4,34 @@ if DEBUG then
     lick.reset = true
 end
 
+require "ship"
+
+function setupShip( W, H )
+    local x = W/2
+    local y = H/2
+    local w = W/6
+    local h = w/2.8
+    local v = {
+        x = W / 1.5,
+        y = H / 0.66
+    }
+    ship = Ship:new( x - w/2, y - h/2, w, h, v, 1 )
+    ship2 = Ship:new( w + 10, h + 10, w, h, v, 2 )
+    ship2.body.color = { 255, 200, 200 }
+end
+
 function love.load( arg ) 
     local opts = {
-        fsaa = 16,
+        fsaa = 8,
         resizable = true,
-        highdpi = true
+        highdpi = true,
     }
     local w = 1680
+    love.window.setTitle( "Experiment - Love2d" )
     if DEBUG then
         opts.display = 2
         w = 1050 
+        love.window.setTitle( love.window.getTitle() .. " - DEBUG" )
     end
     local h = w / 1.69999
     love.window.setMode( w, h, opts)
@@ -22,101 +40,9 @@ function love.load( arg )
     setupShip( love.graphics.getDimensions() )
 end
 
-function setupShip( W, H )
-    if DEBUG then
-        print( "Creating ship for resolution " .. W .. "x" .. H )
-    end
-    stick = {
-        mode = "fill",
-        color = { 215, 144, 66 },
-        x = W/2,
-        y = H/2,
-        w = W/50,
-        h = H * 2
-    }
-    stick.x = stick.x - stick.w/2
-    ship = { 
-        mode = "fill",
-        color = { 205, 205, 205 },
-        x = stick.x,
-        y = stick.y,
-        w = W/8,
-        h = W/1.6999/16,
-        v = {
-            x = W / 1.5,
-            y = H / 0.66
-        },
-        multiplier = 3
-    }
-    shipFin = {
-        mode = "fill",
-        color = { 255, 30, 30 },
-        x = stick.x,
-        y = stick.y,
-        w = ship.w / 4,
-        h = ship.h * 3/4
-    }
-    shipWing = {
-        mode = "fill",
-        color = { 255, 30, 30 },
-        x = ship.x + ship.w/2,
-        y = ship.y + ship.h/2,
-        w = ship.w / 2,
-        h = ship.h / 8
-    }
-    shipWing.x = shipWing.x - shipWing.w/2
-    shipWing.y = shipWing.y - shipWing.h/2
-
-    wheels = {
-        mode = "fill",
-        innerColor = { 255, 30, 30 },
-        outerColor = { 30, 30, 255 },
-        outerRad = ship.h / 2,
-        innerRad = ship.h / 4,
-        locations = {
-            {
-                x = ship.x,
-                y = ship.y + ship.h
-            }, {
-                x = ship.x + ship.w,
-                y = ship.y + ship.h
-            }
-        }
-    }
-    wheels.locations[1].x = wheels.locations[1].x + wheels.outerRad/2 + 10
-    wheels.locations[2].x = wheels.locations[2].x - wheels.outerRad/2 - 10
-end
-
 function love.update( dt )
-    local g = love.joystick.getJoysticks()[1]
-    local dx = g:getGamepadAxis("leftx")
-    local dy = g:getGamepadAxis("lefty")
-    local multiplier = ship.multiplier * g:getGamepadAxis("triggerright") + 1
-
-    if math.abs(dx) < DEAD_ZONE then
-        dx = 0
-    end
-    if math.abs(dy) < DEAD_ZONE then
-        dy = 0
-    end
-
-    stick.x = stick.x + (dx * dt * ship.v.x * multiplier)
-    stick.y = stick.y + (dy * dt * ship.v.y * multiplier)
-
-    ship.x = stick.x - ship.w/2 + stick.w/2
-    ship.y = stick.y - ship.h/2
-
-    shipFin.x = ship.x
-    shipFin.y = ship.y
-
-    shipWing.x = ship.x + ship.w/2 - shipWing.w/2
-    shipWing.y = ship.y + ship.h/2 - shipWing.h/2
-
-    wheels.locations[1].x = ship.x + wheels.outerRad/2 + 10
-    wheels.locations[1].y = ship.y + ship.h
-
-    wheels.locations[2].x = ship.x + ship.w - wheels.outerRad/2 - 10
-    wheels.locations[2].y = ship.y + ship.h
+    ship:update( dt )
+    ship2:update( dt )
 end
 
 function love.resize( w, h )
@@ -124,28 +50,7 @@ function love.resize( w, h )
 end
 
 function love.draw()
-    love.graphics.setColor( stick.color )
-    love.graphics.rectangle( stick.mode, stick.x, stick.y, stick.w, stick.h )
-
-    love.graphics.setColor( ship.color )
-    love.graphics.rectangle( ship.mode, ship.x, ship.y, ship.w, ship.h )
-
-    love.graphics.setColor( shipWing.color )
-    love.graphics.rectangle( shipWing.mode, shipWing.x, shipWing.y, shipWing.w, shipWing.h )
-
-    love.graphics.setColor( shipFin.color )
-    love.graphics.polygon( shipFin.mode,
-        shipFin.x, shipFin.y,
-        shipFin.x + shipFin.w, shipFin.y,
-        shipFin.x, shipFin.y - shipFin.h
-    )
-
-    love.graphics.setColor( wheels.outerColor )
-    love.graphics.circle( wheels.mode, wheels.locations[1].x, wheels.locations[1].y, wheels.outerRad, 100 )
-    love.graphics.circle( wheels.mode, wheels.locations[2].x, wheels.locations[2].y, wheels.outerRad, 100 )
-
-    love.graphics.setColor( wheels.innerColor )
-    love.graphics.circle( wheels.mode, wheels.locations[1].x, wheels.locations[1].y, wheels.innerRad, 100 )
-    love.graphics.circle( wheels.mode, wheels.locations[2].x, wheels.locations[2].y, wheels.innerRad, 100 )
+    ship:draw()
+    ship2:draw()
 end
 
---
+++ ship.lua
@@ -0,0 +1,123 @@
+Ship = {}
+
+function Ship:new( x, y, w, h, v, gamepad )
+    s = {}
+    setmetatable( s, self )
+    self.__index = self
+
+    s.x = x
+    s.y = y
+    s.w = w
+    s.h = h
+    s.v = v
+    s.multiplier = 3
+    s.gamepad = gamepad or 1
+
+    s.body = {
+        mode = "fill",
+        color = { 205, 205, 205 },
+        x = 0, y = 0,
+        w = 1, h = 1
+    }
+
+    s.stick = {
+        mode = "fill",
+        color = { 215, 144, 66 },
+        x = w/2,
+        y = h/2,
+        w = 1/12, -- of self.w
+        h = 600 -- of self.h
+    }
+    s.stick.x = s.stick.x - s.stick.w * s.w/2
+
+    s.fin = {
+        mode = "fill",
+        color = { 255, 30, 30 },
+        x = 0, y = 0,
+        w = 0.25, h = 0.75
+    }
+
+    s.wing = {
+        mode = "fill",
+        color = { 255, 30, 30 },
+        w = 0.5, h = 0.125
+    }
+    s.wing.x = w/2 - s.wing.w * w/2
+    s.wing.y = h/2 - s.wing.h * h/2
+
+    s.wheels = {
+        mode = "fill",
+        innerColor = { 255, 30, 30 },
+        outerColor = { 30, 30, 255 },
+        outerRad = h * 0.5,
+        innerRad = h * 0.25,
+        locations = {
+            { x = 0, y = h },
+            { x = w, y = h }
+        }
+    }
+    s.wheels.locations[1].x = s.wheels.innerRad
+    s.wheels.locations[2].x = w - s.wheels.innerRad
+    s.wheels = nil
+
+    return s
+end
+
+function Ship:draw()
+    love.graphics.push()
+    love.graphics.origin()
+
+    love.graphics.translate( self.x, self.y )
+
+    function drawRect( item )
+        love.graphics.setColor( item.color )
+        love.graphics.rectangle( item.mode, item.x, item.y, item.w * self.w, item.h * self.h )
+    end
+    drawRect( self.stick )
+    drawRect( self.body )
+    drawRect( self.wing )
+
+    love.graphics.setColor( self.fin.color )
+    love.graphics.polygon( self.fin.mode,
+        self.fin.x, self.fin.y,
+        self.fin.x + (self.fin.w * self.w), self.fin.y,
+        self.fin.x, self.fin.y - (self.fin.h * self.h)
+    )
+
+    if self.wheels ~= nil then
+        love.graphics.setColor( self.wheels.outerColor )
+        love.graphics.circle( self.wheels.mode, self.wheels.locations[1].x, self.wheels.locations[1].y, self.wheels.outerRad, 100 )
+        love.graphics.circle( self.wheels.mode, self.wheels.locations[2].x, self.wheels.locations[2].y, self.wheels.outerRad, 100 )
+
+        love.graphics.setColor( self.wheels.innerColor )
+        love.graphics.circle( self.wheels.mode, self.wheels.locations[1].x, self.wheels.locations[1].y, self.wheels.innerRad, 100 )
+        love.graphics.circle( self.wheels.mode, self.wheels.locations[2].x, self.wheels.locations[2].y, self.wheels.innerRad, 100 )
+    end
+
+    love.graphics.pop()
+end
+
+function Ship:update( dt )
+    local g = love.joystick.getJoysticks()[self.gamepad]
+    if g ~= nil then
+        if g:isGamepadDown( "a", "b", "x", "y", "back", "start" ) then
+            self.x = love.window.getWidth()/2 - self.w/2
+            self.y = love.window.getHeight()/2 - self.h/2
+        end
+
+        local dx = g:getGamepadAxis("leftx")
+        local dy = g:getGamepadAxis("lefty")
+        local multiplier = self.multiplier * g:getGamepadAxis("triggerright") + 1
+
+        if math.abs(dx) < DEAD_ZONE then
+            dx = 0
+        end
+        if math.abs(dy) < DEAD_ZONE then
+            dy = 0
+        end
+
+        self.x = self.x + (dx * dt * self.v.x * multiplier)
+        self.y = self.y + (dy * dt * self.v.y * multiplier)
+    end
+end
+
